753c0e0cec6e6e65708b37c1b2bb33c169659858
souer23 <estebannu2005@gmail.com>
2024-10-28 01:26:16 -0300

    Actualizacion

--- app/src/main/java/com/example/pawmeal/MainActivity.kt
+++ app/src/main/java/com/example/pawmeal/MainActivity.kt
@@ -9,14 +9,21 @@ import android.widget.Button
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
-
 class MainActivity : AppCompatActivity() {
+    // Array de pares para almacenar usuarios y contraseñas
+    private val credenciales = arrayOf(
+        Pair("usuario1", "1234"),
+        Pair("usuario2", "abcd"),
+        Pair("usuario3", "5678")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { vista, insets ->
+            val barrasDelSistema = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            vista.setPadding(barrasDelSistema.left, barrasDelSistema.top, barrasDelSistema.right, barrasDelSistema.bottom)
             insets
         }
 
@@ -27,8 +34,8 @@ class MainActivity : AppCompatActivity() {
                 val contraseñaIngresada = findViewById<TextInputEditText>(R.id.etContraseña).text.toString()
 
                 // Validación de las credenciales
-                if (usuarioIngresado == "usuario" && contraseñaIngresada == "1234") {
-                    // Inicio de sesión exitoso, navegar a vista_principal
+                if (validarCredenciales(usuarioIngresado, contraseñaIngresada)) {
+                    // Inicio de sesión exitoso, navegar a VistaPrincipal
                     val intent = Intent(this, VistaPrincipal::class.java)
                     startActivity(intent)
                 } else {
@@ -37,8 +44,17 @@ class MainActivity : AppCompatActivity() {
                 }
             } catch (e: Exception) {
                 // Manejo de errores
-                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun validarCredenciales(usuario: String, contraseña: String): Boolean {
+        for (credencial in credenciales) {
+            if (credencial.first == usuario && credencial.second == contraseña) {
+                return true // Credenciales válidas
+            }
+        }
+        return false // Credenciales inválidas
     }
 }
